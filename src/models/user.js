527253614499aa97b77024e4a793b7547f81a360
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-10-08 18:25:19 +0530

    Add coins field for user

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
   userId: String,
   name: String,
   email: String,
-  password: String
+  password: String,
+  coins: Number
 });
 
 const User = mongoose.model('User', userSchema);
