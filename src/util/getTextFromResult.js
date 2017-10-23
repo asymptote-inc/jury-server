59dca17035c3e96b7e3137d754a799a22f1d12bb
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-10-23 08:01:01 +0530

    Change getTextFromResult to print error

--- src/util/getTextFromResult.js
+++ src/util/getTextFromResult.js
@@ -1,6 +1,10 @@
 // Removes headers etc. and sends only the text.
 const getTextFromResult = callback => (err, res) => {
   if (err) {
+    console.log(err);
+    if (err.message) {
+      console.log(err.message);
+    }
     callback(err);
   } else {
     callback(undefined, res.text);
