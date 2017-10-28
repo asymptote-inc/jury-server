1f0d74028d6b50e21a597d4fb91209625e527e8b
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-10-28 14:35:11 +0530

    Fix leaderboard reverse sorting issue

--- src/xapi/getScoreboard.js
+++ src/xapi/getScoreboard.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 function getScoreboard(callback) {
   User.find()
-    .sort('field coins')
+    .sort('-coins')
     .limit(15)
     .select('name coins -_id')
     .exec((err, res) => {
