0a7b50b64534086f8099c44140e8b988357b7a60
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-10-29 22:26:13 +0530

    Set everyone to display in the leaderboard

--- src/xapi/getScoreboard.js
+++ src/xapi/getScoreboard.js
@@ -3,7 +3,6 @@ const User = require('../models/user');
 function getScoreboard(callback) {
   User.find()
     .sort('-coins')
-    .limit(30)
     .select('name coins -_id')
     .exec((err, res) => {
       if (err) {
