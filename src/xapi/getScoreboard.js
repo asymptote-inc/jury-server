5e218f830a7b579184a285d001c14c78186f48dd
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-10-29 17:57:54 +0530

    Increase scoreboard limit to 30

--- src/xapi/getScoreboard.js
+++ src/xapi/getScoreboard.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 function getScoreboard(callback) {
   User.find()
     .sort('-coins')
-    .limit(15)
+    .limit(30)
     .select('name coins -_id')
     .exec((err, res) => {
       if (err) {
